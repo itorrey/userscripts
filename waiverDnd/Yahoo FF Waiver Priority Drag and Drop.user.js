c188aac449617272e8f0f31731c48f26c4c2a5a5
Torrey Rice <rice@itorrey.com>
2014-11-21 22:05:55 -0800

    Adding update URL
    Signed-off-by: Torrey Rice <rice@itorrey.com>

--- waiverDnd/Yahoo FF Waiver Priority Drag and Drop.user.js
+++ waiverDnd/Yahoo FF Waiver Priority Drag and Drop.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Yahoo FF Waiver Priority Drag and Drop
-// @namespace    
+// @namespace
 // @version      0.1
 // @description  Sort Yahoo waiver priority using DnD
+// @downloadURL  https://github.com/itorrey/userscripts/raw/master/waiverDnd/Yahoo%20FF%20Waiver%20Priority%20Drag%20and%20Drop.user.js
 // @author       Torrey Rice
 // @match        http://football.fantasysports.yahoo.com/*
 // @grant        GM_addStyle
@@ -13,12 +14,12 @@ var testData = '<tr class="first odd yui3-dd-drop" id="yui_3_10_3_2_141645357559
 GM_addStyle(".Table tbody tr { cursor: move; transition:background 0.4s;} .Table tbody tr:hover { background:rgba(0,0,0,0.4); } .Table tbody .yui3-dd-drop-active-valid { background:rgba(73, 79, 54, 0.35);} .yui3-dd-dragging { background:rgba(81, 123, 23, 0.2); } .Table tbody tr td:nth-child(2) { width:107px; } .Table tbody tr td:nth-child(3) { width:169px; } td.priority { width:165px; } ");
 
 YUI().use('sortable', 'selector-css3', function (Y) {
-     
+
     var waiverNodes = Y.all('.Table tbody tr');
      if(!waiverNodes._nodes.length) {
 		Y.one('.Table tbody').append(testData);
      }
-     
+
 	var sortable = new Y.Sortable({
 		container: '.Table tbody',
 		nodes: 'tr',
@@ -28,7 +29,7 @@ YUI().use('sortable', 'selector-css3', function (Y) {
     sortable.delegate.after('drag:end', function (e) {
         var selectNodes = Y.all('.Table tbody tr select');
 		var optionNodes = Y.all('.Table tbody tr select option[selected]').removeAttribute("selected");;
-        
+
         selectNodes.each(function(node, index) {
            var opt = node._node.children[index];
            opt.setAttribute("selected", "true");
